2c9b2eea903dfe6e79c0f3f0527857661f02bfef
Pierre RB <oprbguitar@gmail.com>
2013-06-19 14:20:24 -0500

    home site

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -1,6 +1,8 @@
 Store::Application.routes.draw do
+  
+  get "home/index"
   resources :products
-  root :to => 'products#index'
+  root :to => 'home#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
